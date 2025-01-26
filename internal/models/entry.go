f5b426a8023ac8b985644c757ab54d79c22bbbaa
ayden <aydenboyko@gmail.com>
2025-01-26 02:40:19 -0500

    added time field to entries

--- internal/models/entry.go
+++ internal/models/entry.go
@@ -1,7 +1,12 @@
 package internal
 
+import (
+	"time"
+)
+
 type Entry struct {
 	Id        uint64 `json:"id"`
 	Base62_id string `json:"short_url"`
 	LongUrl   string `json:"long_url"`
+	Date_Created time.Time `json:"date_created"`
 }
