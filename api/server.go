d0814852f4408110cb245c77407b9f70d920cf37
ayden boyko <aydenboyko@gmail.com>
2025-02-04 00:52:21 -0500

    todos

--- api/server.go
+++ api/server.go
@@ -90,7 +90,7 @@ func (s *Server) checkCache(next http.Handler) http.Handler { // TODO save entry
 	})
 }
 
-func (s *Server) LogAction(next http.Handler) http.Handler { // TODO set up logs
+func (s *Server) LogAction(next http.Handler) http.Handler { // TODO set up logs USE .log files
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		log.Printf("Request received: %s %s", r.Method, r.URL.Path)
 		next.ServeHTTP(w, r)
